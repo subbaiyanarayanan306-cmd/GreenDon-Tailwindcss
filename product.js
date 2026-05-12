20c8c4a0e256282bffbec7c5c390244a6aa32639
subbaiyanarayanan306-cmd <subbaiyanarayanan306@gmail.com>
2026-05-13 05:20:37 +0530

    Changes

--- product.js
+++ product.js
@@ -11,3 +11,23 @@ icon.addEventListener("click",()=>{
 closeicon.addEventListener("click",()=>{
      sidenav.style.right="-50%";
 })
+
+// PRODUCT CONTAINER :
+var productContainer=document.getElementById("product-container");
+var search = document.getElementById("search");
+var productlist = productContainer.querySelectorAll("div");
+console.log(productlist)
+
+      search.addEventListener("keyup", () => {
+        var enterValue = event.target.value.toUpperCase();
+        console.log(enterValue);
+
+        for (count = 0; count < productlist.length; count++) {
+          var productname=productlist[count].querySelector("h2").textContent
+          if (productname.toUpperCase().indexOf(enterValue) < 0) {
+            productlist[count].style.display = "none";
+          } else {
+            productlist[count].style.display = "block";
+          }
+        }
+      });
